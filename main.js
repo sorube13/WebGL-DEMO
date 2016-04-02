3bb489cb1956ef203b058f29570a7e4c653b65d1
sorube13 <sorube@gmail.com>
2016-04-02 21:03:45 +0200

    more organized

--- main.js
+++ main.js
@@ -1,53 +1,19 @@
-/*=================Creating a canvas=========================*/var canvas = document.getElementById("myCanvas");
+/*=================Creating a canvas=========================*/
 var canvas = document.getElementById('myCanvas');
 var gl = initWebGL(canvas);
-
-/*===========Defining and storing the geometry==============*/
-
-var vertices = [
-	-0.6, 0.1, 0.0,
-	0.0, 0.7, 0.0,
-	-0.6, 0.7, 0.0,
-	0.0, 0.1, 0.0,
-];
-
-var colors = [ 0,0,1, 1,0,0, 0,1,0, 1,0,1]
-
-var indices = [0, 1, 2, 1, 0, 3];
-/*var vertices = [ -1,-1,-1, 1,-1,-1, 1, 1,-1 ];
-var colors = [ 1,1,1, 1,1,1, 1,1,1 ];
-var indices = [ 0,1,2 ];*/
-
-// Create an empty buffer object and store vertex data
-var vertex_buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-//gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-
-// Create an empty buffer object and store Index data
-var Index_Buffer = gl.createBuffer();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW)
-//gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-// Create an empty buffer object and store color data
-var color_buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
 
 /*========================Shaders============================*/
 // Vertex shader source code
 var vertCode =
-'attribute vec3 coordinates;' + 
-'uniform mat4 Pmatrix;' + 
-'uniform mat4 Vmatrix;' + 
-'uniform mat4 Mmatrix;' +
-'attribute vec3 color;' + 
+'attribute vec2 coordinates;' + 
+'uniform vec2 u_resolution;' + 
+'uniform vec3 color;' + 
 'varying vec3 vColor;' +
 'void main(void) {' + 
-' gl_Position = Pmatrix * Vmatrix * Mmatrix * vec4(coordinates, 1.0);' +
+' vec2 zeroToOne = coordinates / u_resolution;' + 
+' vec2 zeroToTwo = zeroToOne * 2.0;' +
+' vec2 clipSpace = zeroToTwo - 1.0;' +
+' gl_Position = vec4(clipSpace * vec2(1, -1),0.0, 1.0);' +
 ' vColor = color;' + 
  '}';
 
@@ -78,126 +44,39 @@ gl.linkProgram(shaderProgram);
 // Use the combined shader program object
 gl.useProgram(shaderProgram);
 
+/*===========Defining and storing the geometry==============*/
+
+
+// Create an empty buffer object and store vertex data
+var vertex_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+setRectangle(gl, randomInt(300),randomInt(300),randomInt(300),randomInt(300));
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
 
 /* ===========Associating shaders to buffer objects============*/
 
-var Pmatrix = gl.getUniformLocation(shaderProgram, 'Pmatrix');
-var Vmatrix = gl.getUniformLocation(shaderProgram, 'Vmatrix');
-var Mmatrix = gl.getUniformLocation(shaderProgram, 'Mmatrix');
+// Resize canvas (0,0) -> (300,300) 
+var resolutionLocation = gl.getUniformLocation(shaderProgram, "u_resolution");
+gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
 
-//Bind vertex buffer object
+// Set a random color.
+var color = gl.getUniformLocation(shaderProgram, "color");
+gl.uniform3f(color, Math.random(), Math.random(), Math.random());
+
+// set vertex to coordinates
 gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-
-//gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
-
-//Get the attribute location
 var coord = gl.getAttribLocation(shaderProgram, "coordinates");
-gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(coord);
+gl.enableVertexAttribArray(coord)
+gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
 
-// bind the color buffer
-gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+// Create rectangle
+setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
 
-// get the attribute location
-var color = gl.getAttribLocation(shaderProgram, "color");
-gl.vertexAttribPointer(color, 3, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(color);
+/*=================Drawing the triangle and transforming it========================*/
 
-
-/* ==========MATRIX======================================*/
-
-function get_projection(angle, a, zMin, zMax){
-	var ang = Math.tan((angle*.5)*Math.PI/180); 
-	/*return [
-	   0.5/ang, 0 , 0, 0,
-       0, 0.5*a/ang, 0, 0,
-       0, 0, -(zMax+zMin)/(zMax-zMin), -1,
-       0, 0, (-2*zMax*zMin)/(zMax-zMin), 0
-	];*/
-	return [
-		1, 0, 0, 0,
-		0, 1, 0, 0, 
-		0, 0, 1, -1,
-		0, 0, 0, 1]
-}
-
-var proj_matrix = get_projection(40, canvas.width/canvas.height, 1, 100);
-var mov_matrix = [1,0,0,0,	0,1,0,0, 0,0,1,0, 0,0,0,1];
-var view_matrix = [1,0,0,0,	0,1,0,0, 0,0,1,0, 0,0,0,1];
-
-// translating z
-view_matrix[14] = view_matrix[14] - 2; //zoom
-
-/* ==========rotation======================================*/
-function rotateZ(m, angle){
-	var c = Math.cos(angle);
-	var s = Math.sin(angle);
-	var mv0 = m[0], mv4 = m[4], mv8 = m[8];
-
-	/*m[0] = c*m[0] - s*m[1];
-	m[4] = c*m[4] - s*m[5];
-	m[8] = c*m[8] - s*m[9];
-	m[1] = c*m[1] + s*mv0;
-	m[5] = c*m[5] + s*mv4;
-	m[9] = c*m[9] + s*mv8*/
-
-	m[0] = c*m[0] - s*m[2];
-	m[4] = c*m[4] - s*m[6];
-	m[8] = c*m[8] - s*m[10];
-	m[2] = c*m[2] + s*mv0;
-	m[6] = c*m[6] + s*mv4;
-	m[10] = c*m[10] + s*mv8
-
-}
-
-/* ==========Drawing======================================*/
-
-var time_old = 0;
-var animate = function(time){
-	var dt = time - time_old;
-	rotateZ(mov_matrix, dt*0.002);
-	time_old = time;
-
-	gl.enable(gl.DEPTH_TEST);
-	gl.depthFunc(gl.LEQUAL);
-	gl.clearColor(0.9, 0.5, 0.5, 0.9);
-	gl.clearDepth(1.0);
-	gl.viewport(0.0, 0.0, canvas.width, canvas.height);
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-	gl.uniformMatrix4fv(Pmatrix, false, proj_matrix);
-	gl.uniformMatrix4fv(Vmatrix, false, view_matrix);
-	gl.uniformMatrix4fv(Mmatrix, false, mov_matrix);
-
-	gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-	window.requestAnimationFrame(animate);
-}
-
-animate(0);
-
-
-/* ==========translation======================================*/
-/*var Tx = -0.5, Ty = 0.5, Tz = 0.0;
-var translation = gl.getUniformLocation(shaderProgram, 'translation');
-gl.uniform4f(translation, Tx, Ty, Tz, 0.0);
-*/
-
-/* ==========scaling======================================*/
-/*var Sx = 0.5, Sy = 1.5, Sz = 1.0;
-var xformMatrix = new Float32Array([
-	Sx, 	0.0,	0.0,	0.0,
-	0.0,	Sy,	    0.0,	0.0,
-	0.0,	0.0,	Sz,	    0.0,
-	0.0,	0.0,	0.0,	1.0
-]);
-
-var u_xformMatrix = gl.getUniformLocation(shaderProgram, 'u_xformMatrix');
-gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix);*/
-
-/*=================Drawing the riangle and transforming it========================*/
-
-/*// Clear the canvas (default background color)
-gl.clearColor(1, 0.5, 1, 0.9);
+// Clear the canvas (default background color)
+gl.clearColor(0, 1, 1, 0.9);
 
 // Enable the depth test
 gl.enable(gl.DEPTH_TEST); 
@@ -209,7 +88,38 @@ gl.clear(gl.COLOR_BUFFER_BIT);
 gl.viewport(0,0,canvas.width,canvas.height);
 
 // Draw the triangle
-gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);*/
+//gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+
+/*=================GEOMETRY========================*/
+// Returns a random integer from 0 to range - 1.
+function randomInt(range) {
+  return Math.floor(Math.random() * range);
+}
+ 
+
+function setRectangle(gl, x, y, width, height){
+	var x1 = x;
+	var x2 = x + width;
+	var y1 = y;
+	var y2 = y + height;
+	var z = 0;
+
+	var vertices = [
+		x1, y1,
+		x2, y1,
+		x1, y2,
+		x2, y1,
+		x2, y2,
+		x1, y2,];
+
+	gl.bufferData(
+		gl.ARRAY_BUFFER,
+		new Float32Array(vertices),
+		gl.STATIC_DRAW);
+}
+
 
 /*=================Initialize WebGL ========================*/
 
---
+++ main3.js
@@ -0,0 +1,192 @@
+/*=================Creating a canvas=========================*/
+var canvas = document.getElementById('myCanvas');
+var gl = initWebGL(canvas);
+
+/*===========Defining and storing the geometry==============*/
+
+//var vertices = [0, 0, 0,	0, 1, 0,	1, 0, 0];
+
+
+//var colors = [ 0,0,0, 0,0,0, 0,0,0,  0,0,0, 0,0,0, 0,0,0, ]
+
+// Create an empty buffer object and store vertex data
+/*var vertex_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+for(var ii = 0; ii<50; ++ii){
+	setRectangle(
+		gl, randomCanvas(0), -0.5+ii/50, 1, 1);
+}
+//gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+*/
+
+
+// Create an empty buffer object and store color data
+/*var color_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+*/
+
+/*========================Shaders============================*/
+// Vertex shader source code
+var vertCode =
+'attribute vec2 coordinates;' + 
+'uniform vec2 u_resolution;' + 
+'uniform vec3 color;' + 
+'varying vec3 vColor;' +
+'void main(void) {' + 
+' vec2 zeroToOne = coordinates / u_resolution;' + 
+' vec2 zeroToTwo = zeroToOne * 2.0;' +
+' vec2 clipSpace = zeroToTwo - 1.0;' +
+' gl_Position = vec4(clipSpace * vec2(1, -1),0.0, 1.0);' +
+' vColor = color;' + 
+ '}';
+
+//Create a vertex shader object
+var vertShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertShader, vertCode);
+gl.compileShader(vertShader);
+
+//Fragment shader source code
+var fragCode =
+ 'precision mediump float;' + 
+ 'varying vec3 vColor;' + 
+ 'void main(void) {' + 
+ 'gl_FragColor = vec4(vColor, 1.0);' + 
+ '}';
+
+// Create fragment shader object
+var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragShader, fragCode);
+gl.compileShader(fragShader);
+
+// Create a shader program object to store combined shader program
+var shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vertShader); 
+gl.attachShader(shaderProgram, fragShader);
+gl.linkProgram(shaderProgram);
+
+// Use the combined shader program object
+gl.useProgram(shaderProgram);
+
+
+/* ===========Associating shaders to buffer objects============*/
+
+//Bind vertex buffer object
+//gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+
+//gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+
+//Get the attribute location
+var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+//gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+//gl.enableVertexAttribArray(coord);
+
+// bind the color buffer
+//gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+
+// get the attribute location
+/*var color = gl.getAttribLocation(shaderProgram, "color");
+gl.vertexAttribPointer(color, 3, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(color);
+*/
+/*=================1 Drawing the triangle and transforming it========================*/
+// Clear the canvas (default background color)
+gl.clearColor(0, 1, 1, 0.9);
+
+// Enable the depth test
+gl.enable(gl.DEPTH_TEST); 
+
+// Clear the color buffer bit
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+// Set the view port
+gl.viewport(0,0,canvas.width,canvas.height);
+
+var resolutionLocation = gl.getUniformLocation(shaderProgram, "u_resolution");
+gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
+
+var color = gl.getUniformLocation(shaderProgram, "color");
+
+var buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.enableVertexAttribArray(coord)
+gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+
+// draw 50 random rectangles in random colors
+for (var ii = 0; ii < 50; ++ii) {
+// Setup a random rectangle
+	setRectangle(
+	    gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
+
+	// Set a random color.
+	gl.uniform3f(color, Math.random(), Math.random(), Math.random());
+
+	// Draw the rectangle.
+	gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+}
+
+/*=================Drawing the triangle and transforming it========================*/
+/*
+// Clear the canvas (default background color)
+gl.clearColor(1, 0.5, 1, 0.9);
+
+// Enable the depth test
+gl.enable(gl.DEPTH_TEST); 
+
+// Clear the color buffer bit
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+// Set the view port
+gl.viewport(0,0,canvas.width,canvas.height);
+
+// Draw the triangle
+//gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+gl.drawArrays(gl.TRIANGLES, 0, 6);
+*/
+/*=================Initialize WebGL ========================*/
+
+function initWebGL(canvas) {
+	gl = null;
+	try {
+		// Try to grab the standard context. If it fails, fallback to experimental.
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+	}catch(e) {}
+	// If we don't have a GL context, give up now
+	if (!gl) {
+		alert("Unable to initialize WebGL. Your browser may not support it.");
+		gl = null;
+	}
+	return gl;
+}
+
+/*=================GEOMETRY========================*/
+// Returns a random integer from 0 to range - 1.
+function randomInt(range) {
+  return Math.floor(Math.random() * range);
+}
+ 
+
+function setRectangle(gl, x, y, width, height){
+	var x1 = x;
+	var x2 = x + width;
+	var y1 = y;
+	var y2 = y + height;
+	var z = 0;
+
+	var vertices = [
+		x1, y1,
+		x2, y1,
+		x1, y2,
+		x2, y1,
+		x2, y2,
+		x1, y2,];
+
+	gl.bufferData(
+		gl.ARRAY_BUFFER,
+		new Float32Array(vertices),
+		gl.STATIC_DRAW);
+}
+
+
